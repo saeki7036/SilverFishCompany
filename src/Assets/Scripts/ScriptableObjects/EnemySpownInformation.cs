2a9d81dbe6770c01c43c9935c9cfc503e26714d9
saeki7036 <114328780+saeki7036@users.noreply.github.com>
2025-06-30 22:00:06 +0900

    生産パラメータ調整 大砲射程調整 スポーンシステムをWave型に変更

---
+++ src/Assets/Scripts/ScriptableObjects/EnemySpownInformation.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemySpownInformation", menuName = "Scriptable Objects/EnemySpownInformation")]
+public class EnemySpownInformation : ScriptableObject
+{
+    [SerializeField]
+    List<GameObject> EnemyPrehabList;
+
+    [SerializeField]
+    List<EnemyWave> WaveList;
+
+    public int EnemySpownALLValue()
+    {
+        int count = 0;
+        foreach (EnemyWave wave in WaveList)
+            count += wave.GetSpownCountValue();
+
+        return count;
+    }
+
+    public int GetWaveCount() => WaveList.Count;
+
+    public  int GetEnemyIDMax() => EnemyPrehabList.Count;
+
+    public EnemyWave GetWave(int index)
+    {
+        if(index < 0 || index >= WaveList.Count)
+            return null;
+
+        return WaveList[index];
+    }
+
+    public GameObject GetPrehab(int index)
+    {
+        if (index < 0 || index >= EnemyPrehabList.Count)
+            return null;
+
+        return EnemyPrehabList[index];
+    }
+}
+
+[System.Serializable]
+public class EnemyWave
+{
+    [SerializeField]
+    int WaveTimeValue;
+
+    [SerializeField]
+    int EnemySpownValue;
+
+    [SerializeField]
+    List<int> SpownIDList;
+
+    public int GetWaveTime() => WaveTimeValue;
+
+    public int GetSpownCountValue() => EnemySpownValue;
+
+    public List<int> GetSpownID() => SpownIDList;
+
+    public int GetRandomEnemyID()
+    {
+        if (SpownIDList.Count <= 0)
+            return -1;
+
+        return SpownIDList[UnityEngine.Random.Range(0, SpownIDList.Count)];
+    } 
+}
--- src/Assets/Scripts/Test/EnemyManagerTest.cs
+++ src/Assets/Scripts/Test/EnemyManagerTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class EnemyManagerTest : MonoBehaviour
 {
@@ -22,17 +23,38 @@ public class EnemyManagerTest : MonoBehaviour
     [SerializeField]
     public UnityEvent ClearIvent;
 
-    List<EnemyTest> enemyList;
+    [SerializeField]
+    Text EnemyCounter;
 
-    public int timeCount;
+    [SerializeField]
+    EnemySpownInformation enemyInformation;
 
-    public int index = 0;
+    [SerializeField]
+    Text DebugText;
 
     [HideInInspector]
     static EnemyManagerTest instance;
 
     [HideInInspector]
     public static EnemyManagerTest Instance => instance;
+
+    List<EnemyTest> enemyList;
+
+    int RemoveCount = 0;
+
+    int timeCount = 0;
+    
+    int WaveIndex = 0;
+
+    int nextSpownCount = 0;
+
+    int nextIndexCount = 0;
+
+    int currentEnemySpownValue = 0;
+
+    EnemyWave currentWave;
+
+    bool IventOneFlag;
 
     public Vector2 NearestPos(Vector2 basePos ,float range)
     {
@@ -83,18 +105,53 @@ public class EnemyManagerTest : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        IventOneFlag = true;
+        WaveIndex = 0;
+        RemoveCount = 0;
         timeCount = 0;
+        nextIndexCount = 0;
+        nextSpownCount = 0;
         enemyList = new List<EnemyTest>();
+        SetWave();
+        UpdateText();
     }
+
+    void DebugTextRead()
+    {
+        DebugText.text = "timeCount:" + timeCount + "\n" +
+                         "nextSpownCount:" + nextSpownCount + "\n" +
+                         "nextIndexCount:" + nextIndexCount + "\n" +
+                         "currentEnemySpown:" + currentEnemySpownValue + "\n" +                   
+                         "WaveIndex:" + WaveIndex + "\n";                        
+    }
+
+
+    void SetWave()
+    {
+        if (WaveIndex >= enemyInformation.GetWaveCount())
+            return;
+
+        currentWave = enemyInformation.GetWave(WaveIndex);
+        nextIndexCount += currentWave.GetWaveTime();
+        currentEnemySpownValue = currentWave.GetSpownCountValue();
+    }
+
+    Vector2 SpownPos() => new Vector2()
+    {
+        x = UnityEngine.Random.Range(minPos.x, maxPos.x),
+        y = UnityEngine.Random.Range(minPos.y, maxPos.y),
+    };
+
 
     private void Update()
     {
-        enemyList.RemoveAll(enemy => enemy == null);
+        //enemyList.RemoveAll(enemy => enemy == null);
+        DebugTextRead();
+        //Debug.Log(enemyList.Count + " : " + index + " : " + Spowntime.Length);
 
-        Debug.Log(enemyList.Count + " : " + index + " : " + Spowntime.Length);
-
-        if(enemyList.Count == 0 && index == Spowntime.Length) 
+        if (IventOneFlag && RemoveCount == enemyInformation.EnemySpownALLValue()) 
         {
+            IventOneFlag = false;
             ClearIvent?.Invoke();
         }
     }
@@ -107,35 +164,69 @@ public class EnemyManagerTest : MonoBehaviour
 
         timeCount++;
 
-        if (index != Spowntime.Length && Spowntime[index] < timeCount)
+        if(currentEnemySpownValue > 0 && 
+           timeCount >= nextSpownCount)
         {
-            index++;
-            Vector2 pos = new()
-            {
-                x = UnityEngine.Random.Range(minPos.x, maxPos.x),
-                y = UnityEngine.Random.Range(minPos.y, maxPos.y)
-            };
+            nextSpownCount = UnityEngine.Random.Range(nextSpownCount, nextIndexCount);
 
-            GameObject gameObject = Instantiate(
-                EnemyPrehab[UnityEngine.Random.Range(0, EnemyPrehab.Length)],
-                pos,
-                Quaternion.Euler(0f,0f,180f));
+            currentEnemySpownValue--;
 
-            gameObject.transform.parent = transform;
-
-            enemyList.Add(gameObject.GetComponent<EnemyTest>());
+            SpownEnemy();
         }
 
+        if(currentEnemySpownValue <= 0 && 
+           WaveIndex < enemyInformation.GetWaveCount() && 
+           timeCount >= nextIndexCount)
+        {
+            WaveIndex++;
+
+            int beforeIndexCount = nextIndexCount;
+            SetWave();
+            nextSpownCount = UnityEngine.Random.Range(beforeIndexCount,(beforeIndexCount + nextIndexCount / 2));
+        }
+
+        EnemyListUpdate();
+    }
+
+    
+    void SpownEnemy()
+    {
+        int enemyID = currentWave.GetRandomEnemyID();
+
+        if (enemyID == -1) 
+            return;
+
+        GameObject enemyPrehab = enemyInformation.GetPrehab(enemyID);
+
+        if (enemyPrehab == null)
+            return;
+
+        GameObject enemyObject = Instantiate(enemyPrehab, SpownPos(),Quaternion.Euler(0f, 0f, 180f));
+
+        enemyObject.transform.parent = transform;
+
+        enemyList.Add(enemyObject.GetComponent<EnemyTest>());
+    }
+
+    void EnemyListUpdate()
+    {
         for (int i = enemyList.Count - 1; i >= 0; i--)
         {
             if (enemyList[i] == null)
             {
+                RemoveCount++;
                 enemyList.RemoveAt(i);
+                UpdateText();
             }
             else
             {
                 enemyList[i].FixedUpdates();
             }
         }
+    }
+
+    void UpdateText()
+    { 
+        EnemyCounter.text = RemoveCount.ToString() + "/" + enemyInformation.EnemySpownALLValue().ToString();
     }
 }
